cc43f4ded21247323d580ab5d235f1d8578b8188
Noel Arcenio Cortes <noelcorts16@gmail.com>
2024-11-29 19:01:04 +0800

    jkbnklj

--- testgame.c
+++ testgame.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <string.h>
-#include "Redirect.h"
+#include <ctype.h>
+
+int VerificationUsernameIFValid(char *username){
+
+    int len = strlen(username);
+
+
+    if(len < 3 || len >= 20){
+        printf("\nUsername should atleast be 3 characters and not exceeding 20.\n\n");
+        return 0;
+    }
+
+    for(int i = 0; i < len; i++){
+        if(ispunct(username[i])){
+        printf("\nNo special characters such as \"@, #, _, !\".\n\n");
+        return 0;
+        }
+    }
+
+    return 1;
+}
 
 int main(){
 
