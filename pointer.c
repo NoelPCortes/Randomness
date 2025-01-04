aebb3f9b96961a133f9d065d40cc424d15dc5ddf
Noel Arcenio Cortes <noelcorts16@gmail.com>
2025-01-04 19:11:38 +0800

    yes?

--- pointer.c
+++ pointer.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 
-int main() {
-    
-    int num1, num2;
+int isGreater(int *, int *);
+
+int main(){
+
+    int num1, num2, res;
     int *pNum1 = NULL, *pNum2 = NULL;
-    int pSum;
 
-    printf("Input the first number : ");
-    scanf("%d", &num1);
-
-    printf("Input the second number : ");
-    scanf("%d", &num2);
+    printf("Enter two numbers : ");
+    scanf("%d %d", &num1, &num2);
 
     pNum1 = &num1;
     pNum2 = &num2;
-    pSum = *pNum1 + *pNum2;
 
-    printf("The sum of the entered numbers is : %d", pSum);
+    res = (*pNum1 > *pNum2) ? *pNum1 : *pNum2;
 
-    return 0;
+    printf("The greater number is: %d", res);
+    
 }
