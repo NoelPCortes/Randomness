734d6b891fd3febdaecb277dc08e76fa820c4370
Noel Arcenio Cortes <noelcorts16@gmail.com>
2025-01-04 19:33:01 +0800

    findthesquared

--- forFun.c
+++ forFun.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
+#include <math.h>
 
-int power(int x, int y);
-
-int power(int x, int y){
-    int res = 1;
-    for(int i = 1; i <= y; i++){
-        res *= x;
-    }
-    return res;
-}
+int findSquared(int *);
 
 int main(){
 
-    int num1, num2, res = 0;
-    printf("Enter a base, then expo: ");
-    scanf("%d%d", &num1, &num2);
-    res = power(num1, num2);
+    int num = 20;
+    int *pNum = NULL;
+    pNum = &num;
+
+    int res = findSquared(pNum);
+
     printf("Result: %d", res);
+
+}
+
+int findSquared(int *num){
+    return pow(*num, 2);
 }
