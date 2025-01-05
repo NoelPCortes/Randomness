cd1a072ce48e7cdfb5e239f5ab4929f3081a7fce
Noel Arcenio Cortes <noelcorts16@gmail.com>
2025-01-05 17:07:44 +0800

    intermediate callback function pointer

--- forFun.c
+++ forFun.c
@@ -1,20 +1,49 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
-void function(int num){
-    printf("Value of variable: %d\nAddress of Variable %p\n", num, &num);
-}
+int add(int, int);
+int subtract(int, int);
+int multiply(int, int);
+int divide(int, int);
 
 int main(){
 
-    int num;
+    int num1 = rand() % 10 + 1, num2 = rand() % 10 + 1;
 
-    void (*function_pointer)(int);
+    int (*arr_operator[])(int, int) = {add, subtract, multiply, divide};
 
-    function_pointer = &function;
+    int res;
+    res = (*arr_operator[0])(num1, num2);
 
-    printf("Enter a number to be printed: ");
-    scanf("%d", &num);
+    printf("%d + %d = %d\n", num1, num2, res);
 
-    (*function_pointer)(num);
+    res = (*arr_operator[1])(num1, num2);
 
+    printf("%d - %d = %d\n", num1, num2, res);
+
+    res = (*arr_operator[2])(num1, num2);
+
+    printf("%d x %d = %d\n", num1, num2, res);
+
+    res = (*arr_operator[3])(num1, num2);
+
+    printf("%d / %d = %d\n", num1, num2, res);
+
+}
+
+int add(int x, int y){
+    return x + y;
+}
+
+int subtract(int x, int y){
+    return x - y;
+}
+
+int multiply(int x, int y){
+    return x * y;
+}
+
+int divide(int x, int y){
+    return x / y;
 }
