dc7b43e7e7ce0ec6a43067c04965446dab3c2557
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-01-23 20:08:18 +0800

    Print numbers between 1 and 100 with a specific remainder

--- pointer.c
+++ pointer.c
@@ -8,19 +8,17 @@ int main(){
     srand(time(NULL));
 
     int num = rand() % 20 + 5, sum = 0;
+    int a = 25;
 
     printf("Enter a number: ");
     printf("%d\n", num);
 
-    for(int i = 0; i < num; i++){
-        int random = rand() % 20 + 1;
-        if(random % 2 == 0){
-            sum += random;
+    for(int i = 0; i < 100; i++){
+        //int random = rand() % 20 + 1;
+        //printf("%d\n", random);
+        if(i %  a == 3){
+            printf("%d\n", i);
         }
     }
-
-    printf("The sum of the even numbers is: %d\n", sum);
-
-    
 
 }
