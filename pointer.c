bd87c14e9a584d05447c40ead7abd932af0d0d17
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-01-24 20:01:00 +0800

    Populate an array where each element is triple the previous

--- pointer.c
+++ pointer.c
@@ -7,13 +7,13 @@ int main(){
 
     srand(time(NULL));
 
-    int numA = rand() % 20 + 5, numB = rand() % 50 + 5;
+    int numA = rand() % 9 + 1, numB = rand() % 50 + 5;
+    int numX = 5, numY;
     float result = 0;
 
-    for(int i = 1; i <= 50; i++){
-        result += (float)1/i;
+    for(int i = 1; i <= 5; i++){
+        printf("Num[%d]: %d\n", i, numX);
+        numX *= 3;
     }
-
-    printf("Result: %.2lf\n", result);
 
 }
