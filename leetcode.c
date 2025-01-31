e8097831e97264a64a1fc941115a4efa2c31879d
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-01-31 20:33:55 +0800

    after?/

--- leetcode.c
+++ leetcode.c
@@ -8,15 +8,26 @@ int main(){
 
     int students[20];
 
-    int num = rand() % 20 + 1;
+    int num = rand() % 15 + 5;
 
-    for(int i = 0; i < 20; i++){
+    for(int i = 0; i < num; i++){
         students[i] = i;
         printf("Student[%d]\n", students[i] + 1);
     }
 
     printf("%lu\n", sizeof(students));
     printf("%lu\n", sizeof(students)/sizeof(students[0]));
+
+    int *pStudents = NULL;
+    pStudents = (int *)malloc(sizeof(int));
+
+    for(int i = 0; i < num; i++){
+        pStudents[i] = i;
+        printf("Student[%d]\n", pStudents[i] + 1);
+    }
+
+    printf("%lu\n", sizeof(pStudents));
+    printf("%lu\n", sizeof(pStudents)/sizeof(pStudents[0]));
 
     return 0;
 
