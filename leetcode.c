141a1b1e844c6bffb43acf36a2f343ee08efa4b3
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-01-31 20:25:53 +0800

    before

--- leetcode.c
+++ leetcode.c
@@ -4,7 +4,19 @@
 
 int main(){
 
-    
+    srand(time(NULL));
+
+    int students[20];
+
+    int num = rand() % 20 + 1;
+
+    for(int i = 0; i < 20; i++){
+        students[i] = i;
+        printf("Student[%d]\n", students[i] + 1);
+    }
+
+    printf("%lu\n", sizeof(students));
+    printf("%lu\n", sizeof(students)/sizeof(students[0]));
 
     return 0;
 
