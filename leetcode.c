820c6acd18a59cd17e7d7e676dc7acbffe6286bd
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-01-31 21:13:01 +0800

    more coding zzz...

--- leetcode.c
+++ leetcode.c
@@ -1,40 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <stdbool.h>
+
+bool isPalindrome(int);
 
 int main(){
 
-    srand(time(NULL));
+    int x = 121;
 
-    int students[20];
+    bool isTrue = isPalindrome(x);
 
-    int num = rand() % 15 + 5;
-
-    for(int i = 0; i < num; i++){
-        students[i] = i;
-        printf("Student[%d]\n", students[i] + 1);
+    if(isTrue){
+        printf("true\n");
     }
 
-    printf("%lu\n", sizeof(students));
-    printf("%lu\n", sizeof(students)/sizeof(students[0]));
+}
 
-    int *pStudents = NULL;
-    pStudents = (int *)malloc(num * sizeof(int));
-    if(pStudents == NULL){
-        printf("Memory allocaton failed\n");
-        return 1;
+bool isPalindrome(int x) {
+    char *stringArg = (char *)malloc(100 * sizeof(char));
+    int i = 0;
+    if(x < 0){
+        stringArg[i++] = '-';
     }
+    while(x != 0){
+        int *pX = &x;
+        if(*pX % 10 == 0){
+            stringArg[i++] = '0';
+        }
+        stringArg[i++] = x % 10;
+    } 
 
-    for(int i = 0; i < num; i++){
-        pStudents[i] = i;
-        printf("Student[%d]\n", pStudents[i] + 1);
-    }
+    printf("%s\n", stringArg);
 
-    printf("%lu\n", sizeof(pStudents) * num);
-    printf("%d\n", num);
-
-    free(pStudents);
+    free(stringArg);
 
     return 0;
-
 }
