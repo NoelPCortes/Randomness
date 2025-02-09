246957cd7f263f0cc02180c1e0d5c849222e96e6
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-02-09 21:04:57 +0800

    Decimal to Binary

--- forFun.c
+++ forFun.c
@@ -1,35 +1,29 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
+#include <ctype.h>
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-
-int main()
-{
-    int n;
-    scanf("%d", &n);
-    int d;
-    scanf("%d", &d);
-    int h;
-    scanf("%d", &h);
-
-    // Write an answer using printf(). DON'T FORGET THE TRAILING \n
-    // To debug: fprintf(stderr, "Debug messages...\n");
-
-    int resD = d * 24;
-    int aliveRes = n * h;
-
-    if(aliveRes < resD) {
-        printf("Not enough\n");
-    } else if (aliveRes == resD) {
-        printf("Just enough\n");
-    } else {
-        printf("It is enough\n");
+int main(){
+    
+    int num, i = 0, temp = 0;
+    
+    printf("Enter a decimal number: ");
+    scanf("%d", &num);
+    
+    char strArg[25];
+    strArg[0] = '0';
+    
+    while(num > 0){
+        strArg[i] = (num % 2 == 0) ? '0' : '1';
+        num/=2;
+        i++;
     }
- 
-    return 0;
+    
+    int length = strlen(strArg);
+    
+    printf("Binary equivalent: ");
+    
+    while(length >= 0){
+        printf("%c", strArg[length--]);
+    }
+    
 }
