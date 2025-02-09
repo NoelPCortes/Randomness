995f54e5ef4ad32dc4f6d6fb374a8bdc62a583e3
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-02-09 20:08:56 +0800

    string reversal

--- forFun.c
+++ forFun.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
+#include <string.h>
 
 int main(){
+    
+    char stringArg[50];
+    
+    printf("Enter a string: ");
+    fgets(stringArg, sizeof(stringArg), stdin);
+    stringArg[strlen(stringArg)-1] = '\0';
+    
+    int strLength = strlen(stringArg);
 
-    srand(time(NULL));
-
-    int random_number = rand() % 15 + 5;
-
-    int num[100], min_num[100];
-
-    printf("What is the numbers of integers you wish to input? ");
-    printf("%d", random_number);
-
-    printf("Input numbers: ");
-    for(int i = 0; i < random_number; i++){
-        num[i] = rand() % 20 + 1;
+    while(strLength >= 0){
+        printf("%c", stringArg[strLength]);
+        strLength--;
     }
-
-    for(int i = 0; i < random_number; i++){
-        if(num[i] == num[i+1])
-        min_num[i];
-    }
-
+    
 }
