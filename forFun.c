85060ddd93a11947e468d29ba15ae77725c019f6
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-02-11 20:40:26 +0800

    prime checker

--- forFun.c
+++ forFun.c
@@ -2,22 +2,29 @@
 
 int main(){
     
-    double sales, total_fee;
+    int numArgA, primeCheck = 0;
     
-    scanf("%lf", &sales);
+    scanf("%d", &numArgA);
     
-    if(sales < 10000) {
-        total_fee = sales * 0.02;
-    } else if(sales < 25000) {
-        total_fee = sales * 0.05;
-    } else if(sales < 50000) {
-        total_fee = sales * 0.1;
-    } else if(sales < 75000) {
-        total_fee = sales * 0.13;
+    if(numArgA > 0) {
+        int i, j;
+        for(i = 2; i < numArgA; i++){
+            for(j = 2; j <= 10;j++){
+                if(i * j == numArgA){
+                    primeCheck = 1;
+                    break;
+                }
+            }
+        }
     } else {
-        total_fee = sales * 0.15;
+        printf("Invalid input!");
+        return 0;
     }
     
-    printf("%.2lf", total_fee);
+    if(primeCheck == 0) {
+        printf("Prime!");
+    } else {
+        printf("Not Prime!");
+    }
     
 }
