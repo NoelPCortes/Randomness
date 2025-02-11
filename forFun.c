b953c64fc7c5f2f4ad6ce62d93f31b0f6efc1258
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-02-11 20:54:36 +0800

    updated

--- forFun.c
+++ forFun.c
@@ -23,14 +23,15 @@ int main(){
         scanf("%lf", &amount_entered);
         remaining_balance += amount_entered;
         if(remaining_balance < 0) {
-            printf("Warning: Issuance amount exceeds balance. Transaction declined.\n");
+            printf("Warning: Issuance amount exceeds balance. Transaction declined.\n\n");
             remaining_balance -= amount_entered;
+            printf("Current Balance: Php %.2lf\n", remaining_balance);
         } else if(amount_entered > 0) {
             printf("Transaction: Deposit\n");
             printf("Amount entered: Php %.2lf\n", amount_entered);
             printf("Current Balance: Php %.2lf\n", remaining_balance);
         } else if(amount_entered < 0) {
-            printf("Transaction: Deposit\n");
+            printf("Transaction: Issuance\n");
             printf("Amount entered: Php %.2lf\n", amount_entered);
             printf("Current Balance: Php %.2lf\n", remaining_balance);
         } else {
