3aefb210b0ab969faa1aeda746a4f19364e32da6
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-02-11 20:10:33 +0800

    blood type something

--- forFun.c
+++ forFun.c
@@ -2,29 +2,24 @@
 #include <ctype.h>
 
 int main(){
+    char blood_type;
+    float liter_blood;
+    double total_fee;
     
-    int word; 
-    double fee = 18.50;
-    char delivery_type;
-    
-    delivery_type = getchar();
-    if(delivery_type == '\0'){
-        delivery_type = getchar();
+    blood_type = getchar();
+    if(blood_type == '\0'){
+        blood_type = getchar();
     }
     
-    scanf("%d", &word);
+    scanf("%f", &liter_blood);
     
-    if(delivery_type == 's' || delivery_type == 'S'){
-        fee += 5.00;
-        if(word > 12){
-            fee += (float)(word - 12) * 1.50;
-        }
-    } else {
-        if(word > 12){
-            fee += (float)(word - 12) * 1.50;
-        }
+    switch(blood_type){
+        case 'A': total_fee = liter_blood * 3000.00; break;
+        case 'B': total_fee = liter_blood * 1800.75; break;
+        case 'C': total_fee = liter_blood * 2880.25; break;
+        case 'O': total_fee = liter_blood * 1500.00; break;
     }
     
-    printf("%.2lf", fee);
+    printf("%.2lf", total_fee);
     
 }
