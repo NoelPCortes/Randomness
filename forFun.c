36c73228bb3d2fc6a144d64abc0c0ce8302e90db
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-02-12 18:06:34 +0800

    i forgor

--- forFun.c
+++ forFun.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 
+/*A circular racetrack is composed of four portions: concrete, mud, sand and asphalt. Sly Slick’s car takes 30 seconds to cross the concrete, 55 seconds to cross the mud, 47 seconds to cross the sand and 38 seconds to cross the asphalt. Make a program that would input the TIME (in seconds) and output WHERE in the track Sly’s car is. Assume that the race starts at the beginning of the concrete portion.
+
+*/
+
 int main(){
     
-    int i, j;
+    int time_sec, amount_time = 0;
     
-    for(i = 1; i <= 10; i++){
-        for(j = 1; j <= 10; j++){
-            printf("%3d", i * j);
-        }
-        printf("\n");
+    scanf("%d", &time_sec);
+    
+    amount_time += time_sec;
+    
+    if(amount_time > 170) {
+        amount_time -= 170;
+    }
+    
+    if(amount_time <= 30) {
+        printf("concrete");
+    } else if(amount_time <= 85) {
+        printf("mud");
+    } else if(amount_time <= 132) {
+        printf("sand");
+    } else {
+        printf("asphalt");
     }
     
 }
