088946d56d65ab75e7283388a8fd0b160be7fa1f
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-02-12 18:12:22 +0800

    electric bill

--- forFun.c
+++ forFun.c
@@ -1,29 +1,21 @@
 #include <stdio.h>
 
-/*A circular racetrack is composed of four portions: concrete, mud, sand and asphalt. Sly Slick’s car takes 30 seconds to cross the concrete, 55 seconds to cross the mud, 47 seconds to cross the sand and 38 seconds to cross the asphalt. Make a program that would input the TIME (in seconds) and output WHERE in the track Sly’s car is. Assume that the race starts at the beginning of the concrete portion.
-
-*/
+/*In order to discourage excess electric consumption, an electrical company charges its customers a lower rate of P75 for the first 250 kilowatt-hours and a higher rate of P8.5 for each additional kilowatt-hour. In addition, a 10% surtax is added to the final bill. Write a program that calculates the electrical bill given the number of kilowatt-hours consumed as input. At the end, print the number of kilowatt-hours consumed and the computed bill.*/
 
 int main(){
     
-    int time_sec, amount_time = 0;
+    int watt_hrs;
+    double total_bill = 75;
     
-    scanf("%d", &time_sec);
+    scanf("%d", &watt_hrs);
     
-    amount_time += time_sec;
-    
-    if(amount_time > 170) {
-        amount_time -= 170;
+    if(watt_hrs > 250){
+        total_bill += (watt_hrs - 250) * 8.5;
     }
     
-    if(amount_time <= 30) {
-        printf("concrete");
-    } else if(amount_time <= 85) {
-        printf("mud");
-    } else if(amount_time <= 132) {
-        printf("sand");
-    } else {
-        printf("asphalt");
-    }
+    total_bill += (total_bill*0.1);
+    
+    printf("Kilowatt-hours: %d\n", watt_hrs);
+    printf("Electric bill: Php %.2lf\n", total_bill);
     
 }
