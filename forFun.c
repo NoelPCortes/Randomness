30c3a451ba6f9478b365ea9d35036be106d6e02f
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-02-12 19:14:43 +0800

    chatgpts work NOT MINE

--- forFun.c
+++ forFun.c
@@ -1,33 +1,31 @@
 #include <stdio.h>
 
-/*     if total sales is                  Commission is
-above 0 but below 10,000                 2% of total sales
-at least 10,000 but below 25,000       5% of total sales
-at least 25,000 but below 50,000      10% of total sales
-at least 50,000 but below 75,000      13% of total sales
-at least 75,000 but below 100,000    15% of total sales       
-   at least 100,000                                25% of total sales       */
+int main() {
+    int num, i, isPrime = 1;
 
-int main(){
-    
-    double sales, com_sales;
-    
-    scanf("%lf", &sales);
-    
-    if(sales < 10000) {
-        com_sales = sales * 0.02;
-    } else if(sales < 25000) {
-        com_sales = sales * 0.05;
-    } else if(sales < 50000) {
-        com_sales = sales * 0.1;
-    } else if(sales < 75000) {
-        com_sales = sales * 0.13;
-    } else if(sales < 100000) {
-        com_sales = sales * 0.15;
-    } else {
-        com_sales = sales * 0.25;
+    // Ask the user to input a number
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // Check if the number is less than or equal to 1
+    if (num <= 1) {
+        isPrime = 0;
     }
-    
-    printf("%.2lf", com_sales);
-    
+
+    // Check for factors of num
+    for (i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            isPrime = 0;
+            break;
+        }
+    }
+
+    // Print the result
+    if (isPrime) {
+        printf("%d is a prime number.\n", num);
+    } else {
+        printf("%d is not a prime number.\n", num);
+    }
+
+    return 0;
 }
