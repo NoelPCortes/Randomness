ecfef834595fffa337a6ebc020522272efc16cb3
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-02-13 22:03:46 +0800

    it functions now

--- forFun.c
+++ forFun.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 struct student {
-    char ID[10];
-    char first_Name[10];
-    char last_Name[10];
+    char ID[15];
+    char first_Name[15];
+    char last_Name[15];
     char course[10];
-    char birthdate[8];
+    char birthdate[10];
     int year;
     int age;
 };
@@ -16,26 +16,26 @@ int main() {
 
     printf("Enter student ID: ");
     fgets(student1.ID, sizeof(student1.ID), stdin);
-    student1.ID[strcspn(student1.ID, "\n")] = 0;
+    student1.ID[strlen(student1.ID)-1] = '\0';
 
     printf("Enter student first name: ");
     fgets(student1.first_Name, sizeof(student1.first_Name), stdin);
-    student1.first_Name[strcspn(student1.first_Name, "\n")]  = 0;
+    student1.first_Name[strlen(student1.first_Name)-1] = '\0';
 
     printf("Enter student last name: ");
     fgets(student1.last_Name, sizeof(student1.last_Name), stdin);
-    student1.last_Name[strcspn(student1.last_Name, "\n")] = 0;
+    student1.last_Name[strlen(student1.last_Name) -1] = '\0';
 
-    print("Enter student course: ");
+    printf("Enter student course: ");
     fgets(student1.course, sizeof(student1.course), stdin);
-    student1.course[strcspn(student1.course, "\n")] = 0;
+    student1.course[strlen(student1.course) -1] = '\0';
 
     printf("Enter student birthdate: ");
     fgets(student1.birthdate, sizeof(student1.birthdate), stdin);
-    student1.birthdate[strcspn(student1.birthdate, "\n")] = 0;
+    student1.birthdate[strlen(student1.birthdate) -1] = '\0';
 
     printf("Enter student year: ");
-    scanf("%d", student1.year);
+    scanf("%d", &student1.year);
 
     printf("Enter student age: ");
     scanf("%d", &student1.age);
@@ -47,5 +47,5 @@ int main() {
     printf("Student birthdate: %s\n", student1.birthdate);
     printf("Student year: %d\n", student1.year);
     printf("Student age: %d\n", student1.age);
-
+    
 }
