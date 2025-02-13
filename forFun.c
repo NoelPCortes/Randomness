2b13ec7a389f60afd92ab5ab28417d2221e9930c
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-02-13 21:33:48 +0800

    recursion 1!!

--- forFun.c
+++ forFun.c
@@ -1,31 +1,13 @@
 #include <stdio.h>
 
+int printNumbers(int num);
+
 int main() {
-    int num, i, isPrime = 1;
 
-    // Ask the user to input a number
-    printf("Enter a number: ");
-    scanf("%d", &num);
+    printNumbers(50);
+    
+}
 
-    // Check if the number is less than or equal to 1
-    if (num <= 1) {
-        isPrime = 0;
-    }
-
-    // Check for factors of num
-    for (i = 2; i * i <= num; i++) {
-        if (num % i == 0) {
-            isPrime = 0;
-            break;
-        }
-    }
-
-    // Print the result
-    if (isPrime) {
-        printf("%d is a prime number.\n", num);
-    } else {
-        printf("%d is not a prime number.\n", num);
-    }
-
-    return 0;
+int printNumbers(int num) {
+    printf("%d ", printNumbers(num - 1));
 }
