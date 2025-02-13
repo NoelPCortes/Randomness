860981f8efe7e96ef7dd202b07da102717a707fe
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-02-13 21:57:58 +0800

    finished the inputs of every stuct

--- forFun.c
+++ forFun.c
@@ -15,7 +15,29 @@ int main() {
     struct student student1;
 
     printf("Enter student ID: ");
-    fgets("student1.ID", 10, stdin);
+    fgets(student1.ID, sizeof(student1.ID), stdin);
+    student1.ID[strcspn(student1.ID, "\n")] = 0;
 
+    printf("Enter student first name: ");
+    fgets(student1.first_Name, sizeof(student1.first_Name), stdin);
+    student1.first_Name[strcspn(student1.first_Name, "\n")]  = 0;
+
+    printf("Enter student last name: ");
+    fgets(student1.last_Name, sizeof(student1.last_Name), stdin);
+    student1.last_Name[strcspn(student1.last_Name, "\n")] = 0;
+
+    print("Enter student course: ");
+    fgets(student1.course, sizeof(student1.course), stdin);
+    student1.course[strcspn(student1.course, "\n")] = 0;
+
+    printf("Enter student birthdate: ");
+    fgets(student1.birthdate, sizeof(student1.birthdate), stdin);
+    student1.birthdate[strcspn(student1.birthdate, "\n")] = 0;
+
+    printf("Enter student year: ");
+    scanf("%d", student1.year);
+
+    printf("Enter student age: ");
+    scanf("%d", &student1.age);
 
 }
