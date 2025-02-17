53a5d5c2eb4433be240c175140adad7272564a49
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-02-17 21:35:51 +0800

    ADADADAD

--- forFun.c
+++ forFun.c
@@ -1,27 +1,24 @@
-/*Given the function prototype:  
+#include <stdio.h>
 
-     float convertToCm(float  feet, float  inches);
+int exponent(int numA, int numB);
 
-//define a function that will accept two input parameters: distance in feet and distance in inches.  Convert distance_in_feet to centimeters and the distance in INCHES to centimeters.  Return the total converted distances in centimeters.
+int main() {
 
- //define the function main() to allow the user to input distances in feet and inches and output the returned result from function call to convertToCm */
- 
- #include <stdio.h>
- 
- float convertToCm(float feet, float inches);
- 
- int main(){
-     
-     float inch, feet;
-     
-     scanf("%f %f", &feet, &inch);
-     
-     double res = convertToCm(feet, inch);
-     
-     printf("%.2lf", res);
-     
- }
- 
- float convertToCm(float feet, float inches){
-     return ((feet * 12) + inches) * 2.54;
- }
+    int numArgA, numArgB;
+
+    scanf("%d %d", &numArgA, &numArgB);
+
+    int res = exponent(numArgA, numArgB);
+
+    printf("%d\n", res);
+
+}
+
+int exponent(int numA, int numB){
+    int a;
+    int res = 1;
+    for(a = 1; a <= numB; a++){
+        res *= numA;
+    }
+    return res;
+}
