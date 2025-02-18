e13a560d4c466150eb849ecf50e449cfc2d99cbb
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-02-18 21:15:34 +0800

    functions operator

--- forFun.c
+++ forFun.c
@@ -1,24 +1,48 @@
 #include <stdio.h>
+#include <ctype.h>
 
-int sumOfDigits(int );
+int computeSum(int, int);
+int computeDifference(int, int);
+int computeProduct(int, int);
+float computeQuotient(int, int);
 
-int main() {
-    int numArg;
+int main(){
 
-    scanf("%d", &numArg);
+    char operator;
 
-    int res = sumOfDigits(numArg);
+    int numArgA, numArgB;
 
-    printf("%d\n", res);
+    operator = getchar();
+    if(operator == '\n'){
+        operator = getchar();
+    }
+
+    scanf("%d %d", &numArgA, &numArgB);
+
+    switch(operator){
+        case '+': printf("Sum = %d\n", computeSum(numArgA, numArgB)); break;
+        case '-': printf("Difference = %d\n", computeDifference(numArgA, numArgB)); break;
+        case '*': printf("Product = %d\n", computeProduct(numArgA, numArgB)); break;
+        case '/': printf("Quotient = %.2f\n", computeQuotient(numArgA, numArgB)); break;
+        default: printf("Invalid operator!");
+    }
 
 }
 
-int sumOfDigits(int num){
-    int sum = 0;
-    while(num != 0){
-        int temp = num % 10;
-        num /= 10;
-        sum += temp;
-    }
-    return sum;
+int computeSum(int numA, int numB){
+    return numA + numB;
+}
+
+int computeDifference(int numA, int numB){
+    return numA - numB;
+}
+
+int computeProduct(int numA, int numB){
+    return numA * numB;
+}
+
+float computeQuotient(int numA, int numB){
+    float res;
+    res = (float)numA / numB;
+    return res;
 }
