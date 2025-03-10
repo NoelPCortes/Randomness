b80526bf92cf28514bd99f594cc3f62a40cb4969
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-03-10 21:06:35 +0800

    function pointer

--- forFunOnly/forFun.c
+++ forFunOnly/forFun.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
-#include <ctype.h>
-#include "libraryFun.h"
+
+int computeSum(int *, int *);
 
 int main(){
-    
-    char op;
-    int numA, numB;
-    
-    op = getchar();
-    if(op == '\0'){
-        op = getchar();
-    }
-    
-    scanf("%d", &numA);
-    scanf("%d", &numB);
-    
-    switch(op){
-        case '+': printf("Sum = %d", computeSum(numA,numB)); break;
-        case '-': printf("Difference = %d", computeDifference(numA,numB)); break;
-        case '*': printf("Product = %d", computeProduct(numA,numB)); break;
-        case '/': printf("Quotient = %.2f", computeQuotient(numA,numB)); break;
-        default: printf("Invalid operator!");
-    }
-    
+
+    int num1 = 5, num2 = 10;
+
+    int (*ptrFunc)(int *, int*) = NULL;//Pointer function to computeSum
+    ptrFunc = &computeSum;//Storing the memory address of computeSum function
+
+    printf("Sum of %d and %d is %d\n", num1, num2, (*ptrFunc)(&num1, &num2));//How to call a function pointer
+
+}
+
+int computeSum(int *n1, int *n2){
+    return *n1 + *n2;//return the memory address of n1 and n2
 }
