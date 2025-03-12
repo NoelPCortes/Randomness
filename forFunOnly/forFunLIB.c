caf0848a6b80bc4a9110403a9fb49c0a1ae3a407
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-03-12 20:37:16 +0800

    oddUpCount recursion

--- forFunOnly/forFunLIB.c
+++ forFunOnly/forFunLIB.c
@@ -9,13 +9,17 @@ int main(){
     printf("Enter the value of N: ");
     scanf("%d", &numA);
     
+    printf("Odd numbers from 1 to %d are: ", numA);
+    
     oddUpCount(numA);
     
 }
 
 void oddUpCount(int n1){
-    if(n1 > 0){
+    if(n1 % 2 != 0 && n1 > 0){
         oddUpCount(n1 - 1);
         printf("%d ", n1);
+    } else if(n1 > 0){
+        oddUpCount(n1 - 1);
     }
 }
