e02463694744a9fa0d1610e31066720134c8f5f8
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-03-27 20:53:18 +0800

    array in function

--- forFunOnly/forFunLIB.c
+++ forFunOnly/forFunLIB.c
@@ -1,29 +1,15 @@
 #include <stdio.h>
 
-int main(){
-    
-    int pebis[10] = {1,2,3,4,5,6,7,8,9,10};
-
-    int a, temp;
-
-    for(a = 0; a < 10; a++){
-        if(pebis[a] == 10){
-            temp = pebis[a];
-            pebis[a] = pebis[0];
-            pebis[0] = temp;
-        }
-    }
-
-    for(a = 0; a < 10; a++){
-        printf("%d\n", pebis[a]);
-    }
-
+void strArgs(int* strArg[]){
+    strArg[0] = 0;
 }
 
-/*
-Element 1 = 1;
-Element 2 = 4;
-Element 3 = 9;
-Element 4 = 16;
-Element 5 = 25;
-*/
+int main(){
+
+    int strArg[5] = {1,2,3,4,5};
+
+    //strArgs(strArg);
+
+    printf("strArg[0] = %d\n", strArg[0]);
+
+}
